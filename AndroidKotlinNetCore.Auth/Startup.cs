8da1d11c404dc80aea4911cc280799f8d22c83fe
Kerim ALTINTOP <kerimaltintop@gmail.com>
2021-02-23 00:10:45 +0300

    startup cs degistirildi

--- AndroidKotlinNetCore.Auth/Startup.cs
+++ AndroidKotlinNetCore.Auth/Startup.cs
@@ -29,7 +29,7 @@ namespace AndroidKotlinNetCore.Auth
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
+            //!!
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
