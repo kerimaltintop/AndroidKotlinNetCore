bd29c1be18e02917089fc06e6587a3cb73b418fe
Kerim ALTINTOP <kerimaltintop@gmail.com>
2021-02-27 19:04:52 +0300

    config.cs ayarları yapıldı, client düzenlemeleri yapıldı token ayarları eklendi

--- AndroidKotlinNetCore.Auth/Config.cs
+++ AndroidKotlinNetCore.Auth/Config.cs
@@ -1,8 +1,6 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
+﻿using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace AndroidKotlinNetCore.Auth
@@ -13,6 +11,7 @@ namespace AndroidKotlinNetCore.Auth
         {
             new ApiResource("resource_product_api"){Scopes={"api_product_fullpermission"} },
             new ApiResource("resource_photo_api"){Scopes={"api_photo_fullpermission"} },
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
 
@@ -20,43 +19,62 @@ namespace AndroidKotlinNetCore.Auth
         {
              new ApiScope("api_product_fullpermission","Product API için tüm izinler"),
              new ApiScope("api_photo_fullpermission","Photo API için tüm izinler"),
+             new ApiScope(IdentityServerConstants.LocalApi.ScopeName),
         };
 
         public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
         {
+            new IdentityResources.Email(),
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
-            new IdentityResources.Email(),
         };
 
         public static IEnumerable<Client> Clients => new Client[]
         {
-             // m2m client credentials flow client
              new Client
              {
-                 ClientId = "m2m.client",
-                 ClientName = "Client Credentials Client",
+                 ClientId = "AndroidClient_CC",
+                 ClientName = "AndroidClient CC",
 
                  AllowedGrantTypes = GrantTypes.ClientCredentials,
-                 ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                 ClientSecrets = { new Secret("secret".Sha256()) },
 
-                 AllowedScopes = { "scope1" }
+                 AllowedScopes = { IdentityServerConstants.LocalApi.ScopeName}
              },
 
-             // interactive client using code flow + pkce
+
              new Client
              {
-                 ClientId = "interactive",
-                 ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                 ClientId = "AndroidClient_ROP",
+                 ClientSecrets = { new Secret("secret".Sha256()) },
 
-                 AllowedGrantTypes = GrantTypes.Code,
+                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
 
-                 RedirectUris = { "https://localhost:44300/signin-oidc" },
-                 FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                 PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
+                 //Access token da reflesh token ile birlikte donmesini istiyorsak true veriyoruz
                  AllowOfflineAccess = true,
-                 AllowedScopes = { "openid", "profile", "scope2" }
+
+                 AllowedScopes = {
+                     IdentityServerConstants.StandardScopes.Email,
+                     IdentityServerConstants.StandardScopes.OpenId,
+                     IdentityServerConstants.StandardScopes.Profile,
+                     "api_product_fullpermission",
+                     "api_photo_fullpermission",
+                     
+                     //Refresh Token almak için tanımlıyoruz
+                     IdentityServerConstants.StandardScopes.OfflineAccess,
+                 },
+
+                 //token ömrünü belirtiyoruz 10dakika 10*60
+                 AccessTokenLifetime=600,
+
+                 //Refresh token kaç daha kullanılacagını belirtiyoruz
+                 RefreshTokenUsage=TokenUsage.ReUse,
+
+                 //refresh token omur
+                 RefreshTokenExpiration=TokenExpiration.Absolute,
+                 //Absolute(omuru sonunda biter)  
+                 //Sliding(Refresh token ile omur yenilenir)
+                 AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
              },
         };
     }
