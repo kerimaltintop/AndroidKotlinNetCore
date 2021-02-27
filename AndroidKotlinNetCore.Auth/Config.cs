9c1ea615b592e9bfc405af280b6e139576660991
Kerim ALTINTOP <kerimaltintop@gmail.com>
2021-02-27 13:03:32 +0300

    conifg dosyası ayarları yapıldı, ApiScopes altından permission'lar belirlendi,apiResources altında uygulamaların kullananacağı izinler belirlendir

--- AndroidKotlinNetCore.Auth/Config.cs
+++ AndroidKotlinNetCore.Auth/Config.cs
@@ -9,50 +9,55 @@ namespace AndroidKotlinNetCore.Auth
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
+        public static IEnumerable<ApiResource> apiResources => new ApiResource[]
+        {
+            new ApiResource("resource_product_api"){Scopes={"api_product_fullpermission"} },
+            new ApiResource("resource_photo_api"){Scopes={"api_photo_fullpermission"} },
+        };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
 
-        public static IEnumerable<Client> Clients =>
-            new Client[]
-            {
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+             new ApiScope("api_product_fullpermission","Product API için tüm izinler"),
+             new ApiScope("api_photo_fullpermission","Photo API için tüm izinler"),
+        };
 
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email(),
+        };
 
-                    AllowedScopes = { "scope1" }
-                },
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+             // m2m client credentials flow client
+             new Client
+             {
+                 ClientId = "m2m.client",
+                 ClientName = "Client Credentials Client",
 
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                 AllowedGrantTypes = GrantTypes.ClientCredentials,
+                 ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
-                    AllowedGrantTypes = GrantTypes.Code,
+                 AllowedScopes = { "scope1" }
+             },
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+             // interactive client using code flow + pkce
+             new Client
+             {
+                 ClientId = "interactive",
+                 ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
-                },
-            };
+                 AllowedGrantTypes = GrantTypes.Code,
+
+                 RedirectUris = { "https://localhost:44300/signin-oidc" },
+                 FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+                 PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+                 AllowOfflineAccess = true,
+                 AllowedScopes = { "openid", "profile", "scope2" }
+             },
+        };
     }
 }
