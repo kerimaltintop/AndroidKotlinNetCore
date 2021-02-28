4d06d9e65edb33badf33805b5703f9321aca6b5a
Kerim ALTINTOP <kerimaltintop@gmail.com>
2021-02-28 15:51:24 +0300

    SignUp Methodu yazıldı, SignUpView Model Olusturuldu

--- AndroidKotlinNetCore.Auth/Controllers/UserController.cs
+++ AndroidKotlinNetCore.Auth/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AndroidKotlinNetCore.Auth.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,17 +11,40 @@ using static IdentityServer4.IdentityServerConstants;
 
 namespace AndroidKotlinNetCore.Auth.Controllers
 {
+    [Authorize(LocalApi.PolicyName)]
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
-        [Authorize(LocalApi.PolicyName)]
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
         public IActionResult Test()
         {
             return Ok("test ok kerim.");
         }
 
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel signUpViewModel)
+        {
+            var user = new ApplicationUser();
+            user.UserName = signUpViewModel.Username;
+            user.Email = signUpViewModel.Email;
+            user.City = signUpViewModel.City;
 
+            var result = await _userManager.CreateAsync(user, signUpViewModel.Password);
 
+            if (!result.Succeeded)
+            {
+                //Hata mesajı gönderilecek
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
     }
 }
---
+++ AndroidKotlinNetCore.Auth/Models/SignUpViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AndroidKotlinNetCore.Auth.Models
+{
+    public class SignUpViewModel
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
