6345a0b0ce57564050feac3cbe1640f0a5619ca6
Kerim ALTINTOP <kerimaltintop@gmail.com>
2021-02-28 23:56:11 +0300

    IResourceOwnerPasswordValidator interface’nin implement edilmesi

---
+++ AndroidKotlinNetCore.Auth/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,34 @@
+﻿using AndroidKotlinNetCore.Auth.Models;
+using IdentityModel;
+using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AndroidKotlinNetCore.Auth.Services
+{
+    public class IdentityResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public IdentityResourceOwnerPasswordValidator(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var existUser = await _userManager.FindByEmailAsync(context.UserName);
+            if (existUser == null) return;///Error DTO Donulenecek
+
+
+            var passwordCheck = await _userManager.CheckPasswordAsync(existUser, context.Password);
+            if (passwordCheck == false) return;///Error DTO Donulenecek
+
+            context.Result = new GrantValidationResult(existUser.Id.ToString(), OidcConstants.AuthenticationMethods.Password);
+
+
+        }
+    }
+}
